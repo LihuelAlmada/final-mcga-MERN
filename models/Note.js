af94dacf1af106a03bf7e2222cdd59fdbe0c7959
Necralma <lihuel.almada@gmail.com>
2020-12-12 11:26:36 -0300

    Schema Note

---
+++ models/Note.js
@@ -0,0 +1,19 @@
+import {Schema, model} from 'mongoose';
+//model without ID
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    }
+}, {
+    versionKey: false,
+    timestamps: true
+});
+const Note = model('Note', noteSchema);
+export default Note;
