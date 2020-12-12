d2b24d6146744cea4fc00f1972d0951f2ada152c
MatiasForconi <matiasfernandoforconi@hotmail.com>
2020-12-12 16:35:06 -0300

    added client setup

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://German:1234@cluster-ts.aqydr.mongodb.net/mcga-ts?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://Pablo:135@mcga-mern.jgidy.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
 //mongodb+srv://German:1234@cluster-ts.aqydr.mongodb.net/mcga-ts?retryWrites=true&w=majority
