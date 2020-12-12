e65cf893336bbe9ceff07a14c0b94581695a2440
MatiasForconi <matiasfernandoforconi@hotmail.com>
2020-12-12 19:37:40 -0300

    added css in components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
+import React, { Component } from 'react';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Form } from 'react-final-form';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <Form
+            onSubmit={() => {
+                alert("submitting!");
+            }}
+        >
+            {({handleSubmit}) => (
+                <form className="frm" onSubmit={handleSubmit}>
+                  <h2 className="header">Sign In</h2>
+                  <div>
+                    <label>First Name</label>
+                    <input placeholder="Name" type="text" />
+                  </div>
+                  <div>
+                      <label>Phone</label>
+                    <input placeholder="Phone" type="text" />
+                    </div>
+                    <button className='submit' type="submit">Submit</button>
+                </form>
+            )}
+        </Form>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,24 @@
+/*import React from "react";
+import { Form } from "react-final-form";
+
+const noteList = () => {
+    return (
+        <Form
+            onSubmit={() => {
+                alert("submitting!");
+            }}
+        >
+            {({handleSubmit}) => (
+                <form onSubmit={handleSubmit}>
+                    <input placeholder="Name" type="text" />
+                    <input placeholder="Email" type="email" />
+                    <button type="submit">Submit</button>
+                </form>
+            )}
+        </Form>
+    );
+}
+
+export default noteList;
+
+*/
