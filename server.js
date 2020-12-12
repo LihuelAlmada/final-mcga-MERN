ea27a3d1fc4726cc2cefb67e9cf82fd86b09af01
Necralma <lihuel.almada@gmail.com>
2020-12-12 11:07:30 -0300

    Fist commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const mongoose = require('mongoose');
+//para instansias post request, podes tenes un nombre del post
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
