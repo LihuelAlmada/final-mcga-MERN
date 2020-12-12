0c7a2ebe0ea0668ab00cc5481815ff7c62e2db78
Necralma <lihuel.almada@gmail.com>
2020-12-12 11:20:42 -0300

    server can on

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://German:1234@cluster-ts.aqydr.mongodb.net/mcga-ts?retryWrites=true&w=majority'
+}
+//mongodb+srv://German:1234@cluster-ts.aqydr.mongodb.net/mcga-ts?retryWrites=true&w=majority
--- server.js
+++ server.js
@@ -3,4 +3,20 @@ const mongoose = require('mongoose');
 //para instansias post request, podes tenes un nombre del post
 const bodyParser = require('body-parser');
 
+const app = express();
+
+//Middleware
 app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongodb
+mongoose
+    .connect(db)
+    .then(()=> console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`));
